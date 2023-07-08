16715df48764835380222404e23a543960602c53
Kamran Khan Alwi <beingkamrankhanalwi@gmail.com>
2023-07-08 23:37:39 +0530

    edit modal

--- src/components/EditEmployee.js
+++ src/components/EditEmployee.js
@@ -1,10 +1,12 @@
 import { useState } from 'react';
-import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 
-function EditEmployee() {
-  const [show, setShow] = useState(false);
+function EditEmployee(props) {
+  const [name, setName] = useState(props.name);
+  const [role, setRole] = useState(props.role);
 
+  const [show, setShow] = useState(false);
+  
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
@@ -26,32 +28,56 @@ function EditEmployee() {
           <Modal.Title>Update Employee</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-        <form class="w-full max-w-sm">
-        <div class="md:flex md:items-center mb-6">
-            <div class="md:w-1/3">
-            <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="name">
+        <form id='editmodel' className="w-full max-w-sm">
+        <div className="md:flex md:items-center mb-6">
+            <div className="md:w-1/3">
+            <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="role">
                 Full Name
             </label>
             </div>
-            <div class="md:w-2/3">
-            <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="name" type="text" value="Jane Doe" />
+            <div className="md:w-2/3">
+              <input 
+                className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" 
+                id="name" 
+                type="text" 
+                value={name}
+                onChange={(e) => {
+                  setName(e.target.value);
+                }}
+                
+              />
             </div>
             </div>
-            <div class="md:flex md:items-center">
-                <div class="md:w-1/3"></div>
-                <div class="md:w-2/3">
-                <button class="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
-                    Sign Up
-                </button>
-                </div>
+            <div className="md:flex md:items-center mb-6">
+            <div className="md:w-1/3">
+            <label 
+              className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" 
+              for="role">
+                Role
+            </label>
+            </div>
+            <div className="md:w-2/3">
+            <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" 
+              id="role" 
+              type="text" 
+              value={role} 
+              onChange={(e) => {
+                setRole(e.target.value);
+              }}
+            />
+            </div>
             </div>
         </form>
         </Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
+          <button className='bg-gray-400 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded' 
+          onClick={handleClose}
+          >
             Close
-          </Button>
-          <Button variant="primary">Update</Button>
+          </button>
+
+          <button className='bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded' 
+            form='editmodal'>Update</button>
         </Modal.Footer>
       </Modal>
     </>
